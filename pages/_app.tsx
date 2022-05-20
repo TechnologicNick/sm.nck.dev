1bdbc1196009d152bc1f6c02a189da37bb112f59
TechnologicNick <technologicnick@gmail.com>
2022-05-20 21:26:17 +0200

    Add NextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import useDarkMode from "use-dark-mode";
+
+const lightTheme = createTheme({
+	type: "light",
+	theme: {
+		colors: {
+
+		},
+	},
+});
+	
+const darkTheme = createTheme({
+	type: "dark",
+	theme: {
+		colors: {
+
+		},
+	},
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const darkMode = useDarkMode(true);
+
+  return (
+    <NextUIProvider theme={darkMode.value ? darkTheme : lightTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
 export default MyApp;
