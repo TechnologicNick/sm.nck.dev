d92823743911c8661f6a1374cf84272cbaf5c5d9
TechnologicNick <technologicnick@gmail.com>
2022-06-11 21:10:25 +0200

    Move GenericData import to beforeAll

--- projects/save-editor/structures/__tests__/generic-data.test.ts
+++ projects/save-editor/structures/__tests__/generic-data.test.ts
@@ -9,9 +9,13 @@ import Uuid from "../uuid";
 import type { IGenericData } from "./../generic-data";
 
 describe("GenericData", () => {
-  test("deserialize", async () => {
-    const { default: GenericData } = await import("./../generic-data");
+  let GenericData: typeof import("./../generic-data").default;
 
+  beforeAll(async () => {
+    GenericData = (await import("./../generic-data")).default;
+  })
+
+  test("deserialize", async () => {
     const buffer = Buffer.from("67ce7fe2f7564898b8f076080146a358000401000000fffe03000000307300023f3851ea0001002336200900f0100000003f920d28bf529608011000010add16f30000000100000002ffffffff", "hex");
     const actual = GenericData.deserialize(buffer);
 
@@ -27,8 +31,6 @@ describe("GenericData", () => {
   });
 
   test("serialize", async () => {
-    const { default: GenericData } = await import("./../generic-data");
-
     const expected = Buffer.from("67ce7fe2f7564898b8f076080146a358000401000000fffe03000000307300023f3851ea0001002336200900f0100000003f920d28bf529608011000010add16f30000000100000002ffffffff", "hex");
     const genericData = new GenericData({
       uid: Uuid.parse("67ce7fe2-f756-4898-b8f0-76080146a358"),
