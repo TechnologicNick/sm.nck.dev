d0e4c12c1fcf8f5c72b33bdc0b8367d095ca8a0e
TechnologicNick <technologicnick@gmail.com>
2022-07-12 22:59:44 +0200

    Upgrade next to v12.2.2

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
+// @ts-check
+
+/**
+ * @returns {import('next').NextConfig}
+ */
 module.exports = (phase, { defaultConfig }) => {
   return {
     ...defaultConfig,
 
-    experimental: {
-      outputStandalone: true,
-    },
+    output: "standalone",
 
     webpack: (config) => {
       config.resolve = {
         ...config.resolve,
         fallback: {
           "fs": false,
-        }
+        },
       }
       return config
     },
