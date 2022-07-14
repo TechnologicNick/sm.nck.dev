7b918b12a64799c62843712ad0d037854362105d
TechnologicNick <technologicnick@gmail.com>
2022-07-14 17:27:02 +0200

    Fix indentation

--- projects/save-editor/structures/__tests__/user-generated-content.test.ts
+++ projects/save-editor/structures/__tests__/user-generated-content.test.ts
@@ -25,8 +25,8 @@ describe("UserGeneratedContent", () => {
   test("serialize", async () => {
     const expected = Buffer.from("00000000a7cba862947ea54da01a2da59840c81a1722c8f9", "hex");
     const userGeneratedContent = new UserGeneratedContent({
-        fileId: BigInt(2815141986),
-        localId: Uuid.parse("f9c82217-1ac8-4098-a52d-1aa04da57e94"),
+      fileId: BigInt(2815141986),
+      localId: Uuid.parse("f9c82217-1ac8-4098-a52d-1aa04da57e94"),
     });
 
     expect(userGeneratedContent.serialize()).toEqual(expected);
