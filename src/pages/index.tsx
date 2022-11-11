b9331be05f114c326c283a29bd05e58e2ba3bcd8
TechnologicNick <technologicnick@gmail.com>
2022-11-11 23:20:50 +0100

    Fix index page text positioning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,13 +26,13 @@ export default function Home() {
         <title>Scrap Mechanic Projects - nck.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container sm display="flex" justify="center" direction="column" css={{ pt: "$10" }}>
+      <Container sm display="flex" justify="center" alignItems="center" direction="column" css={{ pt: "$10" }}>
         <Text h1>
           Scrap Mechanic Projects
         </Text>
         <Text>
           Projects I've made related to the 2016 videogame {}
-          <Link href="https://store.steampowered.com/app/387990/Scrap_Mechanic/" target="_blank">
+          <Link href="https://store.steampowered.com/app/387990/Scrap_Mechanic/" target="_blank" css={{ display: "inline" }}>
             Scrap Mechanic
           </Link>
         </Text>
