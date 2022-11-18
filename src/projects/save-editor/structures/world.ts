9e63868b5b90d985eb79eed3f89a81ea1d7055b3
TechnologicNick <technologicnick@gmail.com>
2022-11-18 16:36:34 +0100

    Implement World structure

--- src/projects/save-editor/structures/world.ts
+++ src/projects/save-editor/structures/world.ts
@@ -2,20 +2,53 @@ import GenericData, { IGenericData } from "./generic-data";
 import { IDeserializable } from "./deserializable";
 
 export interface IWorld extends IGenericData {
-  
+  seed: number;
+  filename: string;
+  classname: string;
+  terrainParams: string;
 }
 
 export default class World extends GenericData implements IWorld, IDeserializable<IWorld> {
 
-  
+  seed: number = 0;
+  filename: string = "";
+  classname: string = "";
+  terrainParams: string = "";
+
+  static deserializeData(data: Buffer) {
+    let offset = 0x00;
+
+    const readNextLengthString = () => {
+      const length = data.readUInt16BE(offset);
+      offset += 0x02;
+
+      const string = data.subarray(offset, offset + length);
+      offset += length;
+
+      return string.toString();
+    };
+
+    const seed = data.readUInt32BE(0x00);
+    offset += 0x04;
+
+    const filename = readNextLengthString();
+    const classname = readNextLengthString();
+    const terrainParams = readNextLengthString();
+
+    return {
+      seed,
+      filename,
+      classname,
+      terrainParams,
+    }
+  }
 
   static deserialize(buffer: Buffer) {
     const base = super.deserialize(buffer);
 
-    console.log(base);
-
     return new World({
       ...base,
+      ...World.deserializeData(base.data),
     })
   }
 
@@ -24,12 +57,33 @@ export default class World extends GenericData implements IWorld, IDeserializabl
     Object.assign(this, base);
   }
 
+  serializeData() {
+    const buffer = Buffer.alloc(0x04 + 3 * 0x02 + Buffer.byteLength(this.filename) + Buffer.byteLength(this.classname) + Buffer.byteLength(this.terrainParams));
+
+    let offset = 0x00;
+
+    const writeNextLengthString = (string: string) => {
+      const length = Buffer.byteLength(string);
+
+      buffer.writeUInt16BE(length, offset);
+      offset += 0x02;
+
+      buffer.write(string, offset);
+      offset += length;
+    };
+
+    buffer.writeUInt32BE(this.seed, offset);
+    offset += 0x04;
+
+    writeNextLengthString(this.filename);
+    writeNextLengthString(this.classname);
+    writeNextLengthString(this.terrainParams);
+
+    return buffer;
+  }
+
   serialize() {
-    const buffer = Buffer.alloc(0x36);
-
-    
-
-    this.data = buffer;
+    this.data = this.serializeData();
 
     return super.serialize();
   }
