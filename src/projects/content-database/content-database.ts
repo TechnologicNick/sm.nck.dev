66b347814e15472cf54057e25572a357aed96494
TechnologicNick <technologicnick@gmail.com>
2023-01-12 19:52:14 +0100

    Add manifest and file caching

--- src/projects/content-database/content-database.ts
+++ src/projects/content-database/content-database.ts
@@ -2,6 +2,7 @@ import { reloadDescriptions } from "./databases/descriptions";
 import { EResult } from "steam-user";
 import { FileId, SteamCdnFile, SteamCdnManifest } from "./types";
 import { loggedOn, user } from "./steam-client";
+import NodeCache from "node-cache";
 
 const APP_ID = 387990;
 const WORKSHOP_DEPOT = 387990;
@@ -23,7 +24,17 @@ export const ensureDatabaseLoaded = async () => {
   }
 }
 
+const manifestCache = new NodeCache({
+  stdTTL: 60 * 60,
+  useClones: false,
+});
+
 export const getManifestByFileId = async (publishedFileId: FileId) => {
+  const cached = manifestCache.get<SteamCdnManifest>(publishedFileId);
+  if (cached) {
+    return cached;
+  }
+
   await loggedOn;
   const publishedFileDetails = await user.getPublishedFileDetails(publishedFileId);
   const { result, hcontent_file } = publishedFileDetails.files[publishedFileId];
@@ -32,7 +43,28 @@ export const getManifestByFileId = async (publishedFileId: FileId) => {
     throw new Error(`Could not get published file details: ${EResult[result]}`);
   }
 
-  return (await (user as any).getManifest(APP_ID, WORKSHOP_DEPOT, hcontent_file, "public")).manifest as SteamCdnManifest;
+  const manifest = (await (user as any).getManifest(APP_ID, WORKSHOP_DEPOT, hcontent_file, "public")).manifest as SteamCdnManifest;
+  manifestCache.set(publishedFileId, manifest);
+
+  return manifest;
+}
+
+const fileCache = new NodeCache({
+  stdTTL: 60 * 60,
+  useClones: false,
+});
+
+const downloadFile = async (depotId: number, file: SteamCdnFile) => {
+  const cached = fileCache.get<Buffer>(file.sha_content);
+  if (cached) {
+    return cached;
+  }
+
+  await loggedOn;
+  const downloadedFile = (await (user as any).downloadFile(APP_ID, depotId, file)).file as Buffer;
+  fileCache.set(file.sha_content, downloadedFile);
+
+  return downloadedFile;
 }
 
 export const getFileFromManifest = async (manifest: SteamCdnManifest, filename: string) => {
@@ -43,7 +75,7 @@ export const getFileFromManifest = async (manifest: SteamCdnManifest, filename: 
     return null;
   }
 
-  return (await (user as any).downloadFile(APP_ID, manifest.depot_id, file)).file as Buffer;
+  return await downloadFile(manifest.depot_id, file);
 }
 
 export const getFilesFromManifest = async <T extends Readonly<string[]>>(manifest: SteamCdnManifest, files?: T | ((file: SteamCdnFile) => boolean)) => {
@@ -61,7 +93,7 @@ export const getFilesFromManifest = async <T extends Readonly<string[]>>(manifes
   const downloadedFiles: Partial<Record<T[number], Buffer>> = {};
 
   for (const file of filesToDownload) {
-    downloadedFiles[file.filename as keyof typeof downloadedFiles] = (await (user as any).downloadFile(APP_ID, manifest.depot_id, file)).file;
+    downloadedFiles[file.filename as keyof typeof downloadedFiles] = await downloadFile(manifest.depot_id, file);
   }
 
   return downloadedFiles;
