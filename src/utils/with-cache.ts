b29d0e8d7b1e93ef8c2201c0e2bed0bbfaffe58b
TechnologicNick <technologicnick@gmail.com>
2023-07-08 17:35:08 +0200

    Replace `node-cache` with `stale-while-revalidate-cache` in withCache()

--- src/utils/with-cache.ts
+++ src/utils/with-cache.ts
@@ -1,21 +1,29 @@
-import NodeCache, { Key } from "node-cache";
+import { createStaleWhileRevalidateCache } from "stale-while-revalidate-cache";
 
-const withCache = <A extends Array<any>, R>(fn: (...args: A) => Promise<R>, getCacheKey: (...args: A) => Key) => {
-  const cache = new NodeCache({
-    stdTTL: 60 * 60,
-    useClones: false,
+const withCache = <A extends Array<any>, R>(fn: (...args: A) => Promise<R>, getCacheKey: (...args: A) => string | number) => {
+  const store = Object.create(null);
+
+  const swr = createStaleWhileRevalidateCache({
+    storage: {
+      async getItem(cacheKey: string) {
+        return store[cacheKey];
+      },
+      async setItem(cacheKey: string, cacheValue: any) {
+        store[cacheKey] = cacheValue;
+      },
+      async removeItem(cacheKey: string) {
+        delete store[cacheKey];
+      },
+    },
+    minTimeToStale: 60 * 60 * 1000, // 1 hour
+    maxTimeToLive: Infinity,
   });
 
   return async (...args: A): Promise<R> => {
     const cacheKey = getCacheKey?.(...args) ?? args[0];
-    const cached = cache.get<Promise<R>>(cacheKey);
-    if (cached !== undefined) {
-      return await cached;
-    }
 
-    const promise = fn(...args);
-    cache.set(cacheKey, promise);
-    return await promise;
+    const res = await swr(`${cacheKey}`, () => fn(...args));
+    return res.value;
   };
 }
 
