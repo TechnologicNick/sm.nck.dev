d04ad9f94f4aa05c65faff962c10778f0513bc83
TechnologicNick <technologicnick@gmail.com>
2024-02-08 21:33:27 +0100

    Add NavbarV2 Save Editor links and menu

---
+++ src/components/navigation/navbar-links.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { Button } from "@nextui-org/button";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/dropdown";
+import { Link } from "@nextui-org/link";
+import { NavbarItem, NavbarMenuItem } from "@nextui-org/navbar";
+import { PropsOf } from "@nextui-org/system";
+import { usePathname } from "next/navigation";
+import { GoChevronDown } from "react-icons/go";
+
+export const saveEditorDropdownItems = [
+  {
+    key: "game",
+    textValue: "Game",
+    description: "The game mode, seed, game tick and general information about the save file.",
+    href: "/save-editor/game"
+  },
+  {
+    key: "players",
+    textValue: "Players",
+    description: "List of players in the save file, including their name, ID, and position.",
+    href: "/save-editor/players"
+  },
+  {
+    key: "mods",
+    textValue: "Mods",
+    description: "All mods and custom game dependencies used in the save file.",
+    href: "/save-editor/mods"
+  },
+  {
+    key: "worlds",
+    textValue: "Worlds",
+    description: "Table of worlds in the save file, their scripts and script data.",
+    href: "/save-editor/worlds"
+  },
+  {
+    key: "containers",
+    textValue: "Containers",
+    description: "Inventories and items of all parts and players.",
+    href: "/save-editor/containers"
+  },
+] as const satisfies Array<PropsOf<typeof DropdownItem>>;
+
+export const SaveEditorDropdown = () => {
+  return (
+    <Dropdown>
+      <NavbarItem>
+        <DropdownTrigger>
+          <Button
+            disableRipple
+            className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+            endContent={<GoChevronDown />}
+            radius="sm"
+            variant="light"
+            size="lg"
+          >
+            Save Editor
+          </Button>
+        </DropdownTrigger>
+      </NavbarItem>
+      <DropdownMenu
+        aria-label="Save Editor"
+        className="w-[340px]"
+        classNames={{
+          list: "grid [grid-auto-rows:1fr]"
+        }}
+        itemClasses={{
+          base: "px-3 py-4",
+          title: "font-semibold",
+        }}
+        items={saveEditorDropdownItems}
+        disableAnimation
+      >
+        {
+          (item) => (
+            <DropdownItem
+              key={item.key}
+              textValue={item.textValue}
+              description={item.description}
+              href={item.href}
+            >
+              {item.textValue}
+            </DropdownItem>
+          )
+        }
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
+
+type NavbarMenuLinkProps = {
+  href: string;
+  children: React.ReactNode;
+  indentLevel?: number;
+} & PropsOf<typeof NavbarMenuItem>;
+
+const NavbarMenuLink = ({ href, children, indentLevel = 0, ...props }: NavbarMenuLinkProps) => {
+  const pathname = usePathname();
+
+  return (
+    <NavbarMenuItem {...props}>
+      <Link
+        href={href}
+        color={pathname === href ? "primary" : "foreground"}
+        className={pathname === href ? "font-semibold" : ""}
+        style={{ paddingLeft: `${indentLevel * 1.5}rem` }}
+      >
+        {children}
+      </Link>
+    </NavbarMenuItem>
+  );
+}
+
+export const NavbarMenuLinks = () => {
+  return (
+    <>
+      <NavbarMenuLink href="/">Home</NavbarMenuLink>
+      <NavbarMenuLink href="/save-editor">Save Editor</NavbarMenuLink>
+      {saveEditorDropdownItems.map((item) => (
+        <NavbarMenuLink
+          key={item.key}
+          href={item.href}
+          indentLevel={1}
+        >
+          {item.textValue}
+        </NavbarMenuLink>
+      ))}
+    </>
+  );
+}
--- src/components/navigation/navbar-v2.tsx
+++ src/components/navigation/navbar-v2.tsx
@@ -1,19 +1,23 @@
 import { Link } from "@nextui-org/link";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle } from "@nextui-org/navbar";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/navbar";
 import { FaDiscord, FaGithub } from "react-icons/fa";
 import ThemeSwitcher from "./theme-switcher";
+import { NavbarMenuLinks, SaveEditorDropdown } from "./navbar-links";
 
 const NavbarV2 = () => {
   return (
-    <Navbar isBordered>
+    <Navbar
+      isBordered
+      height="3.25rem"
+    >
       <NavbarMenuToggle className="sm:hidden" />
       <NavbarBrand>
         <Link href="/" color="foreground" className="max-sm:hidden font-semibold">
           sm.nck.dev
         </Link>
       </NavbarBrand>
-      <NavbarContent justify="center">
-
+      <NavbarContent justify="center" className="max-sm:hidden">
+        <SaveEditorDropdown />
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="flex">
@@ -30,6 +34,9 @@ const NavbarV2 = () => {
           <ThemeSwitcher className="text-xl text-default-600 dark:text-default-500" />
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu>
+        <NavbarMenuLinks />
+      </NavbarMenu>
     </Navbar>
   );
 }
